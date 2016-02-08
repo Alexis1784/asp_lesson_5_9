b830d8edf07935e3a80c3d44df96cfbaf61b4d8c
Alex <alexis1784@gmail.com>
2016-02-09 04:10:49 +0500

    working version

--- asp_lesson_5_8/Controllers/HomeController.cs
+++ asp_lesson_5_8/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using asp_lesson_5_8.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -44,6 +45,40 @@ namespace asp_lesson_5_8.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Courses = db.Courses.ToList();
+            return View(student);
+        }
+
+        //http://metanit.com/sharp/mvc5/5.10.php
+        [HttpPost]
+        public ActionResult Edit(Student student, int[] selectedCourses)
+        {
+            Student newStudent = db.Students.Find(student.Id);
+            newStudent.Name = student.Name;
+            newStudent.Surname = student.Surname;
+
+            newStudent.Courses.Clear();
+            if (selectedCourses != null)
+            {
+                //получаем выбранные курсы
+                foreach (var c in db.Courses.Where(co => selectedCourses.Contains(co.Id)))
+                {
+                    newStudent.Courses.Add(c);
+                }
+            }
+
+            db.Entry(newStudent).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
