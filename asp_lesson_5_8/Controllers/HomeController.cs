5243c375d3b6a341f6a263c06e58aecfbb9f5fc1
Alex <alexis1784@gmail.com>
2016-02-09 04:02:58 +0500

    have added

---
+++ asp_lesson_5_8/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using asp_lesson_5_8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_lesson_5_8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private StudentsContext db = new StudentsContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        public ActionResult Details(int id = 0)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ asp_lesson_5_8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_lesson_5_8.MvcApplication" Language="C#" %>
